4caac6fe7c4bdd254a0ec4ef9739aefe47e615ba
minimin123 <kmmin727@gmail.com>
2022-03-26 21:36:08 +0900

    :mangosix: feat: 마이페이지

--- src/components/units/myPage/myClasses/appliedClasses/AppliedClasses.styles.ts
+++ src/components/units/myPage/myClasses/appliedClasses/AppliedClasses.styles.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { breakPoints } from '../../../../../commons/styles/media'
 
 export const ClassWrapper = styled.div`
   display: flex;
@@ -7,9 +8,9 @@ export const ClassWrapper = styled.div`
 `
 
 export const ClassContents = styled.div`
-  padding: 60px 13px 60px 8px;
   height: 240px;
   display: flex;
+
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -25,20 +26,51 @@ export const ClassContents = styled.div`
       transform: none;
     }
   }
+  @media ${breakPoints.tablet} {
+    height: 200px;
+  }
+  @media ${breakPoints.mobile} {
+    height: 180px;
+    flex-direction: column;
+    align-items: flex-end;
+    padding-top: 10px;
+  }
 `
 export const ClassContentsLeft = styled.div`
+  width: 75%;
   display: flex;
+  @media ${breakPoints.mobile} {
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-start;
+  }
 `
 export const ClassImage = styled.div`
   width: 180px;
   height: 120px;
   background: #f1f1f1;
+  @media ${breakPoints.tablet} {
+    width: 150px;
+    height: 100px;
+  }
+  @media ${breakPoints.mobile} {
+    margin-top: 30px;
+    width: 120px;
+    height: 80px;
+  }
 `
 export const ClassContentsList = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding: 15px 0px 5px 40px;
+  padding: 15px 0px 5px 20px;
+
+  @media ${breakPoints.tablet} {
+    padding: 5px 0px 0px 10px;
+  }
+  @media ${breakPoints.mobile} {
+    padding: 5px 0px 5px 10px;
+  }
 `
 
 export const ClassContentsListTop = styled.div`
@@ -50,24 +82,67 @@ export const ClassCategory = styled.div`
   font-weight: 400;
   font-size: 16px;
   margin-bottom: 13px;
+  @media ${breakPoints.tablet} {
+    font-size: 15px;
+  }
+  @media ${breakPoints.mobile} {
+    font-size: 14px;
+    margin-bottom: 5px;
+  }
 `
 
 export const ClassTitle = styled.div`
+  width: 100%;
   font-style: normal;
   font-weight: 700;
   font-size: 20px;
+  /* text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap; */
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+  line-height: 1.5rem;
+  height: calc(1.5rem * 2);
+
+  @media ${breakPoints.tablet} {
+    width: 100%;
+    font-size: 15.5px;
+  }
+  @media ${breakPoints.mobile} {
+    font-size: 14px;
+    width: 200px;
+  }
 `
 
 export const ClassDate = styled.div`
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
-  line-height: 10px;
+  @media ${breakPoints.tablet} {
+    font-size: 14px;
+  }
+  @media ${breakPoints.mobile} {
+    font-size: 12px;
+  }
 `
 
 export const ClassContentsRight = styled.div`
+  width: 10%;
   font-weight: 700;
   font-size: 18px;
+  @media ${breakPoints.tablet} {
+    font-size: 15.5px;
+    width: 11%;
+    text-align: right;
+  }
+  @media ${breakPoints.mobile} {
+    width: 90px;
+    font-size: 14px;
+  }
 `
 
 export const ClassPayButton = styled.button`
@@ -83,5 +158,15 @@ export const ClassPayButton = styled.button`
   border: 0px none;
   :hover {
     cursor: pointer;
+  }
+  @media ${breakPoints.tablet} {
+    width: 80px;
+    font-size: 15px;
+  }
+  @media ${breakPoints.mobile} {
+    width: 80px;
+    height: 25px;
+    font-size: 14px;
+    margin: 0px 20px 10px 0px;
   }
 `
